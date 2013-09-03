1022e115fffcaa39375d236c897c7e09651083ab
Twizzlebird <dave@twizzlebird.co.uk>
2013-09-03 10:54:12 +0100

    added play js

--- main.js
+++ main.js
@@ -162,6 +162,10 @@ myselect[0].selectedIndex =0;
 myselect.selectmenu("refresh");
 });
 
+var followspotvideo = document.getElementById('followspotvideo');
+followspotvideo.addEventListener('click',function(){
+  followspotvideo.play();
+},false);
 
 });
 
